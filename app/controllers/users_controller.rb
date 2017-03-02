bb9c981fe28f987f37757bf8182d38bfd1bf3a33
Tran Xuan Nam <tran.xuan.nam@framgia.com>
2017-03-02 13:31:06 +0700

    User login, logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,25 @@
 class UsersController < ApplicationController
+  before_action :load_user, only: :show
+  before_action :correct_user, only: [:edit, :update]
+
+  def index
+    @users = User.order_by_name(true).paginate page: params[:page],
+      per_page: Settings.paginate.user_per_page
+  end
+
   def new
     @user = User.new
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      login @user
+      flash[:success] = t "signup.success"
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,8 +29,17 @@ class UsersController < ApplicationController
   end
 
   private
+  def load_user
+    @user = User.find_by id: params[:id]
+    unless @user
+      flash[:danger] = t "error.user_not_found"
+      redirect_to root_url
+    end
+  end
+
   def user_params
     params.require(:user).permit :user_name, :email, :password,
       :password_confirmation, :image
   end
+
 end
